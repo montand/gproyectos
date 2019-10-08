a61a1e074dacbaf4ecbb628732209e0b3bc14d28
Victor Lopez <victorgera@gmail.com>
2019-10-08 10:58:56 -0500

    Base del proyecto con catproyectos

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,31 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/main', function () {
+    return view('main');
+})->name('main');
+
+Route::view('criterios', 'catcriterios')->name('criterios');
+Route::view('ejercicios', 'catejercicios')->name('ejercicios');
+
+Route::resource('proyectos', 'proyectoController');
+Route::resource('escenarios', 'escenarioController');
+
+Route::view('pmaestro', 'pmaestro.index')->name('pmaestro.index');
+Route::view('configuracion', 'configuracion.index')->name('configuracion.index');
+Route::view('usuarios', 'usuarios.index')->name('usuarios.index');
+// Route::get('proyectos', 'proyectoController@index')->name('proyectos.index');
+// Route::get('proyectos/crear', 'proyectoController@create')->name('proyectos.create');
+// Route::get('proyectos/{proyecto}/editar', 'proyectoController@edit')->name('proyectos.edit');
+// Route::patch('proyectos/{proyecto}', 'proyectoController@update')->name('proyectos.update');
+// Route::get('proyectos/{proyecto}', 'proyectoController@show')->name('proyectos.show');
+// Route::post('proyectos', 'proyectoController@store')->name('proyectos.store');
+// Route::delete('proyectos/{proyecto}', 'proyectoController@destroy')->name('proyectos.destroy');
+
+Auth::routes(['register' => false]);
+// Auth::routes(['verify' => true]);
+
+// Route::get('/home', 'HomeController@index')->name('home');
+// ->middleware('verified');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,13 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css');
+
+mix.scripts([
+    'jquery/dist/jquery.js',
+    'bootstrap/dist/js/bootstrap.js'
+], 'public/js/all.js', 'node_modules');
+
+mix.browserSync({
+    proxy: 'gproyectos.test'
+});
